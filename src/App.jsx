44bcbd6c745d391be43ca3d94019e301adcf64ff
Umesh kumar mahto <umeshkumarmahto1305@gmail.com>
2025-02-22 12:27:03 +0530

    Add to Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,72 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 const App = () => {
+  const [ImageData, setImageData] = useState([]);
+  const [CartData, setCartData] = useState([]);
 
-  const [ImageData, setImageData] = useState([])
-
-  async function ClickBtn(){
-
-    const response = await axios.get('https://fakestoreapi.com/products')
+  async function ClickBtn() {
+    const response = await axios.get("https://fakestoreapi.com/products");
     console.log(response.data);
-    setImageData(response.data)
-    
+    setImageData(response.data);
+    // setCartData(response.data)
+    // console.log(setCartData[response.data]);.
 
+    const CartCopyData = [...CartData];
+    CartCopyData.push([response.data]);
+    setCartData(CartCopyData);
   }
   return (
-    <div className='p-10'>
-    
-      <button onClick={ClickBtn} className='px-3 py-2 active:scale-95 bg-blue-300 rounded-lg'>GET products </button>
+    <div className="p-10">
+      <button
+        onClick={ClickBtn}
+        className=" absolute top-0 px-3 py-2 active:scale-95 bg-blue-300 rounded-lg"
+      >
+        GET products
+      </button>
 
+      {ImageData.map(function (item, idx) {
+        return (
+          <div key={idx} className="w-full h-screen bg-red-100 flex items-center justify-between  gap-4">
+            <div className=" h-screen w-80 bg-red-300">
+              <img
+                className=" h-60  border-2 px-1.5 py-3 border-amber-300"
+                src={item.image}
+                alt={item.title}
+              />
+              <p className="text-red-500">Price: {item.price}</p>
+              <p className="text-blue-500">Category: {item.category}</p>
+              {/* <p>Id: {item.id}</p> */}
+              <button
+                className="px-3 py-2 bg-orange-500  rounded-md mt-3 m-5"
+                onClick={() => addToCart(item)}
+              >
+                Add To Cart
+              </button>
+            </div>
+            {CartData.map(function (item, index) {
+        return (
+          <div key={index} className="flex-nowrap inline-block w-80">
+            <img
+              className=" h-60 w-60 border-2 px-1.5 py-3 border-amber-300"
+              src={item[0].image}
+              alt={item[0].title}
+            />
+            <p className="text-red-500">Price: {item[0].price}</p>
+            <p className="text-blue-500"> Category: {item[0].category}</p>
+            {/* <p>Id: {item[0].id}</p> */}
+            <button className="px-3 py-2 bg-orange-500  rounded-md mt-3 m-5">
+              Remove From Cart
+            </button>
+          </div>
+        );
+      })}
+          </div>
+        );
+      })}
 
-{ImageData.map(function(item,idx){
-  return <div key={idx} className='inline-block '>
-   
-    <img className=' h-60 w-60 border-2 px-1.5 py-3 border-amber-300' src={item.image} alt={item.title} />
-    <p>Price: {item.price}</p>
-    <p>Category: {item.category}</p>
-    <p>Id: {item.id}</p>
-      {/* <h2>{item.title}</h2> 
-     <p>{item.description}</p>  */}
-  </div>
-})}
-
+      
     </div>
   );
-}
+};
 
 export default App;
